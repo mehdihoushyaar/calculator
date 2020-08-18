f9c0c8b78dbf238dca375fa4081be292d16a21dc
Mehdi Houshyaar <mehdihoushyaar@gmail.com>
2020-08-18 13:45:05 +0430

    Updates

---
+++ precache-manifest.b2b6e4e2759095b7aae62c54f9f25258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f23cba130af945298ae8fb55d31085f",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d4a3a7b5cd012bc0769d",
+    "url": "/calculator/static/css/main.c1767feb.chunk.css"
+  },
+  {
+    "revision": "8ef176acd8e0629f693a",
+    "url": "/calculator/static/js/2.9ff0e669.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.9ff0e669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a3a7b5cd012bc0769d",
+    "url": "/calculator/static/js/main.3c0818bf.chunk.js"
+  },
+  {
+    "revision": "fe5f2d7df921ea514d53",
+    "url": "/calculator/static/js/runtime-main.873ca3e3.js"
+  }
+]);
